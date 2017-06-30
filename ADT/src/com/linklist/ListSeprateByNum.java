c7c742f7a212dff7aaea45517f7d8f2e8e844207
nowhere233 <377501462@qq.com>
2017-06-30 17:55:16 +0800

    分隔链表完成

--- ADT/src/com/linklist/ListSeprateByNum.java
+++ ADT/src/com/linklist/ListSeprateByNum.java
@@ -1,10 +1,92 @@
 package com.linklist;
 
+import com.util.LINKLIST;
+import com.util.TNode;
+
 /*
  * 给定Num，将链表中大于Num的放在右边，小于num的结点放在num结点左侧
  */
 public class ListSeprateByNum {
 	
+	public static TNode separateList(TNode head,int num) {
+		if(head == null) return null;
+		TNode leftHead = null,leftRear = null,numHead = null,numRear = null;
+		TNode rightHead = null,rightRear = null;
+		TNode cur = head,tmp,rear = null;
+		while(cur != null) {
+			tmp = cur.next;
+			//处理大于num的结点放在右链表
+			if(cur.val > num) {
+				if(rightHead == null) {
+					rightHead = rightRear= cur;
+					rightRear.next = null; 
+				}else { 
+					rightRear.next = cur;
+					rightRear = cur; 
+					rightRear.next = null;
+				}
+			} 
+			//处理大于num的结点放在右链表
+			if(cur.val == num) {
+				if(numHead == null) {
+					numHead = numRear= cur;
+					numRear.next = null; 
+				}else { 
+					numRear.next = cur;
+					numRear = cur; 
+					numRear.next = null;
+				}
+			}
+			//处理大于num的结点放在右链表
+			if(cur.val < num) {
+				if(leftHead == null) {
+					leftHead = leftRear= cur;
+					leftRear.next = null; 
+				}else { 
+					leftRear.next = cur;
+					leftRear = cur; 
+					leftRear.next = null;
+				}
+			}
+			cur = tmp;
+		}
+		LINKLIST.PRINT_List(leftHead);
+		LINKLIST.PRINT_List(numHead);
+		LINKLIST.PRINT_List(rightHead);
+		if(leftRear != null) {
+			head = leftHead;
+			rear = leftRear;
+		}
+		
+		if(rear != null) {
+			if(numHead != null) {
+				rear.next = numHead;
+				rear = numRear;
+			}
+		}else {
+			head = numHead;
+			rear = numRear;
+		}
+		
+		if(rear != null) {
+			if(rightHead != null) {
+				rear.next = rightHead;
+				rear = rightRear;
+			}
+		}else {
+			head = rightHead;
+			rear = rightRear;
+		}
+		
+		return head;
+	}
 	
-
+	public static void main(String[] args) {
+		int[] a = new int[]{3,8,1,4,2,5,9,10,6};
+		TNode head = LINKLIST.BUILD_LinkList(a);
+		//LINKLIST.PRINT_List(head);
+		head = ListSeprateByNum.separateList(head, 5);
+		LINKLIST.PRINT_List(head);
+	}
+	
 }
