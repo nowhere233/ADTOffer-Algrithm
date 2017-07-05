b50e9ee3b7acc7248f9702d0b88f175e36cb7257
nowhere233 <377501462@qq.com>
2017-07-05 08:51:07 +0800

    实验

--- ADT/src/com/tree/AVLTree.java
+++ ADT/src/com/tree/AVLTree.java
@@ -1,8 +1,29 @@
 package com.tree;
 
+import com.util.TNode;
+
 /*
  * 平衡二叉树
  */
 public class AVLTree {
+	public TNode insert(TNode root) {
+		TNode nRoot = null;
+		
+		return nRoot;
+	}
+	
+	/*
+	 * 左左型旋转
+	 */
+	public TNode LLRotate(TNode root) {
+		return root;
+	}
+	
+	/*
+	 * 右右型旋转
+	 */
+	public TNode RRRotate(TNode root) {
+		return root;
+	}
 	
 }
